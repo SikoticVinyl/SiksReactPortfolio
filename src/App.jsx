672590507a9a6c9c0651867f978caa672379cd19
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-31 19:05:04 -0700

    update app.jsx to layout imports to create.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+//Components
+import Navigation from './components/Navigation'; 
+import AboutMe from './components/AboutMe'; 
+import Projects from './components/Projects'; 
+import PersonalProjects from './components/PersonalProjects';
+import SchoolProjects from './components/SchoolProjects';
+// import FreelanceProjects from './components/FreelanceProjects'; 
+import Contact from './components/Contact';
+import Resume from './components/Resume'; 
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      {/* Your components will be included here */}
-      {/* Example: <Header />, <Navigation />, etc. */}
-    </div>
+    <BrowserRouter>
+      <Navigation /> {/*Navigation component */}
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio/personal" element={<PersonalProjects />} />
+        <Route path="/portfolio/school" element={<SchoolProjects />} />
+        {/*Route for Freelance - Uncomment when ready to use: <Route path="/portfolio/freelance" element={<FreelanceProjects />} />*/}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+        {/* Space for more routes as needed */}
+      </Routes>
+      <Footer /> {/*Footer component */}
+    </BrowserRouter>
   );
 }
 
 export default App;
+
 
