80aba230876b765047663e8a07d02bb54a19035f
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-31 19:42:44 -0700

    added bg img asset and brought via app.css, added header.js missing logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'; // Make sure to create this CSS file
+import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        {/* Insert your logo here, e.g., an <img> tag or <div> with background-image */}
+        {/* logo here*/}
       </div>
-      <h1 className="header-title">Your Name</h1>
+      <h1 className="header-title">Dana Fulmer</h1>
       <nav>
         <ul className="nav-links">
           {/* Using NavLink for active styling */}
-          <li><NavLink to="/projects">Projects</NavLink></li>
           <li><NavLink to="/about">About Me</NavLink></li>
-          <li><NavLink to="/contact">Contact Me</NavLink></li>
           <li><NavLink to="/resume">Resume</NavLink></li>
+          <li><NavLink to="/projects">Projects</NavLink></li>
+          <li><NavLink to="/contact">Contact Me</NavLink></li>
         </ul>
       </nav>
     </header>
