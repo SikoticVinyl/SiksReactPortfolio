da058397043d5a82576fd68990fafc10d62f4df8
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-31 18:50:40 -0700

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your components will be included here */}
+      {/* Example: <Header />, <Navigation />, etc. */}
+    </div>
+  );
+}
+
+export default App;
+
