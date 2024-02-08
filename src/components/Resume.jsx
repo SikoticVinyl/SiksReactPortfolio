4dc3b6b21295c833db08b9fcec36dcf7a2f0d16f
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-08 15:50:14 -0700

    Resume page working, began design portfolio.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,32 +6,38 @@ function Resume() {
     "JavaScript (ES6+)",
     "React",
     "Node.js",
+    "Discord.js",
     "Express",
     "MongoDB",
-    "SQL (MySQL/PostgreSQL)",
+    "SQL (MySQL/noSQL)",
     "Git and GitHub",
     "Responsive Design",
   ];
 
   return (
-    <section className="resume py-8 px-4">
-      <h2 className="text-3xl font-bold mb-4 text-center sm:text-left">Resume</h2>
-      <div className="text-center sm:text-left">
-        <p className="mb-4">You can download my full resume below. Here's a quick overview of my technical skills:</p>
-        <a href={`${PROPER PATH NEEDED}/DanaFulmerTechResume.pdf`} download="DanaFulmerTechResume.pdf" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer mb-8">
-          Download Resume
-        </a>
-      </div>
-      <div className="mt-8">
-        <h3 className="text-2xl font-bold mb-4">Proficiencies</h3>
-        <ul className="list-disc pl-8 text-left">
-          {proficiencies.map((skill, index) => (
-            <li key={index} className="text-lg mb-2">{skill}</li>
-          ))}
-        </ul>
+    <section className="flex flex-col justify-between items-center min-h-screen py-8 px-4">
+      <div className="w-full max-w-2xl flex flex-col items-center space-y-4">
+        <div className="w-full">
+          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4 text-center">Resume</h2>
+        </div>
+        <div className="text-center">
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-4">You can download my full resume below.</p>
+          <a href="/DanaFulmerTechResume.pdf" download="DanaFulmerTechResume.pdf" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer mb-8">
+            Download Resume
+          </a>
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-4">Here's a quick overview of my technical skills:</p>
+        </div>
+        <div className="mt-8">
+          <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-4">Proficiencies</h3>
+          <ul className="list-disc pl-8 text-left">
+            {proficiencies.map((skill, index) => (
+              <li key={index} className="text-md sm:text-lg md:text-xl lg:text-2xl mb-2">{skill}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default Resume;
