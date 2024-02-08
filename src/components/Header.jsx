d901c56ebd00f14cc8aebae2ae2a6d1f13f885a4
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-08 16:29:49 -0700

    adjusted header to be responsive properly, no logo small.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ import Navigation from './Navigation';
 function Header() {
   return (
     <header className="bg-purple-400 flex items-center justify-center py-4 sm:py-6 md:py-8 w-full px-4 relative">
-      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 h-full flex items-center">
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 h-full flex items-center md:inline-flex hidden">
         <img src={LogoImage} alt="Logo" className="h-3/4 w-auto object-contain" />
       </div>
       <div className="flex flex-col items-center text-center z-10">
-        <h1 className="text-dark-purple text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">Dana R Fulmer</h1>
+        <h1 className="text-dark-purple text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">Dana R Fulmer</h1>
         <Navigation />
       </div>
     </header>
