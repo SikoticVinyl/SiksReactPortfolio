9c5d3c6b21ea5baad53283061bf4f9dfc4ad011b
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-08 17:13:44 -0700

    styled footer. Responsive resume page.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,27 @@ import venmoIcon from '../assets/Venmo.png';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-4">
+    <footer className="bg-gray-800 py-8">
       <div className="container mx-auto flex justify-center items-center">
-        <a href="https://www.linkedin.com/in/danafulmerdev">
+        <a href="https://www.linkedin.com/in/danafulmerdev" className="mx-4">
           <img
             src={linkedinIcon}
             alt="LinkedIn"
-            className="w-6 h-6 text-white hover:text-blue-500 mx-2"
+            className="w-12 h-12 text-white hover:text-blue-500"
           />
         </a>
-        <a href="https://github.com/SikoticVinyl">
+        <a href="https://github.com/SikoticVinyl" className="mx-4">
           <img
             src={githubIcon}
             alt="GitHub"
-            className="w-6 h-6 text-white hover:text-blue-500 mx-2"
+            className="w-12 h-12 text-white hover:text-blue-500"
           />
         </a>
-        <a href="https://venmo.com/u/DanaFulmer">
+        <a href="https://venmo.com/u/DanaFulmer" className="mx-4">
           <img
             src={venmoIcon}
             alt="Venmo"
-            className="w-6 h-6 text-white hover:text-blue-500 mx-2"
+            className="w-12 h-12 text-white hover:text-blue-500"
           />
         </a>
       </div>
