496e6ddb5b249269d056ef52dbbbcb06ddddf838
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-08 17:02:40 -0700

    styled resume page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      'light-pink': '#F7C1D3',
-      'dark-purple': '#5D3B8C',
-    },},
+        'light-pink': '#F7C1D3',
+        'dark-purple': '#5D3B8C',
+        'light-blue': '#91C8FF',
+        'soft-lavender': '#CAB8FF',
+      },
+      backgroundColor: {
+        '91C8FF': '#91C8FF',
+        '91C8FF-dark': '#7daecc', // Darkened version of Light Blue for hover state
+        'CAB8FF': '#CAB8FF',
+      },
+      textColor: {
+        'light-pink': '#F7C1D3',
+        'dark-purple': '#5D3B8C',
+      }
+    },
   },
   variants: {},
   plugins: [],
